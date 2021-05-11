d71ed2880c1735b26c62450dafebf5235ca90e19
xanf-code <darshanaswath@gmail.com>
2021-05-11 23:04:21 +0530

    moon

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import InsiderPage from '@/views/InsiderPage.vue'
+import '../../node_modules/nprogress/nprogress.css';
+import NProgress from 'nprogress';
 
 const routes = [
   {
@@ -21,6 +23,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
+
+router.beforeResolve((to, from, next) => {
+  if (to.name) {
+    NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false });
+    NProgress.start()
+  }
+  next()
+});
+
+
+router.afterEach(() => {
+  NProgress.done()
+});
 
 export default router
